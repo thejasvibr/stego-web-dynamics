6989f29d10911f6d8cf2a81a9c04f85013d55ec8
thejasvi <thejasvib@gmai.com>
2020-06-23 10:03:13 +0200

    1) implement mean analysis with/out day 1 and single spiders 2) run median nparLD analysis 3) report results into main report

--- poresize_CN_nparLD.R
+++ poresize_CN_nparLD.R
@@ -10,6 +10,9 @@
 # source('poresize_CN_nparLD.R')
 
 library(nparLD)
+
+## MEAN DATA nparLD analysis - which TB thinks is not very reliable because the mean is very outlier sensitive. 
+
 porsizedata<- read.csv('poresize_nparLD.csv')
 poresize_formula <- poresize~days*groupsize
 res_poresize<-nparLD(poresize_formula,data=porsizedata,subject="webidnum",  description=T,
@@ -28,5 +31,58 @@ print('CN analysis done')
 
 save(res_cn, res_poresize, file='results_CNandPoresize_nparld.RData')
 
+### D and H suspect that most of the variation comes from the first day and when the spiders are still 'settling' into the web construction.
+# What happens if we remove day 1?
+
+## Poresize without day 1 
+porsize_woday1 <- subset(porsizedata, days>1)
+woday1_poresize_results <-  nparLD(poresize_formula,data=porsize_woday1,subject="webidnum", 
+                                description=T,
+                                group1.order=c(1,5,10,25))
+
+## CN without day 1 
+cn_woday1 <- subset(cn_data, days>1)
+woday1_cn_results <- nparLD(cn_formula,data=cn_woday1,subject="webidnum", 
+                        description=T,
+                        group1.order=c(1,5,10,25))
+
+save(woday1_poresize_results, woday1_cn_results, file='results_CNandPoresize_woday1_nparld.RData')
+
+# CN + poresize without spider group size 1 because they contribute most of the variation
+cn_wo1spider <- subset(cn_data, groupsize>1)
+wo1spider_res <- nparLD(cn_formula,data=cn_wo1spider,subject="webidnum", 
+                        description=T,
+                        group1.order=c(5,10,25))
+
+poresize_wo1spider <- subset(porsizedata, groupsize>1)
+wo1spider_poresize_results <-  nparLD(poresize_formula,data=poresize_wo1spider,subject="webidnum", 
+                                description=T,
+                                group1.order=c(5,10,25))
+
+save(wo1spider_res, wo1spider_poresize_results, file='results_CNandPoresize_wo1spider_nparld.RData')
+
+
+### SUMMARY of Mean data analysis
+# It doesn't matter if you remove day 1 or groupsize 1, there's still a lot of variation. This likely because 
+# of the inherent outlier sensitivity of the the mean as a measure.
+
+
+# MEDIAN DATA ANALYSIS 
+median_data <- read.csv('median_poresize_cn.csv')
+tilld5_mediandata <- subset(median_data, day<6)
+
+results_med_poresize <- nparLD(median_poresize~day*group_size,data=tilld5_mediandata, subject="web_id", 
+                                description=T,
+                                group1.order=c(1,5,10,25))
+
+results_median_CN <- nparLD(median_CN~day*group_size, data=tilld5_mediandata, subject="web_id", 
+                        description=T,
+                        group1.order=c(1,5,10,25))
+
+what_was_done<-'The median data was analysed with nparLD on 23-06-2020'
+results_median_CN$description <- what_was_done
+results_med_poresize$description <- what_was_done
+
+save(results_med_poresize, results_median_CN, file='results_MEDIAN_CNandPoresize_nparld.RData')
 
 
